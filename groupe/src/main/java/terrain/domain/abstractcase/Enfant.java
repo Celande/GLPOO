3913ffbe004b9bf484655ef810c14af0c345f9f9
Celande <celande.adrien@gmail.com>
2017-03-31 19:31:49 +0200

    Implémentation de la méthode equals(Enfant)
    Voir les commentaires pour le parcours de le liste de déplacements

--- groupe/src/main/java/terrain/domain/abstractcase/Enfant.java
+++ groupe/src/main/java/terrain/domain/abstractcase/Enfant.java
@@ -1,12 +1,13 @@
 package terrain.domain.abstractcase;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Enfant extends AbstractCase {
 	
 	private String nom;
 	private Orientation  orientation;
-	private List<Deplacement> deplacement;
+	private List<Deplacement> deplacements;
 	
 	public Enfant(Orientation orientation, List<Deplacement> deplacements, String string) {
 		// TODO Auto-generated constructor stub
@@ -29,6 +30,25 @@ public class Enfant extends AbstractCase {
 	
 	public List<Deplacement> getDeplacements(){
 		
-		return deplacement;
+		return deplacements;
+	}
+	
+	public boolean equals(Enfant enfant){
+		if(this.nom != enfant.getNom())
+			return false;
+		
+		if(this.orientation != enfant.getOrientation())
+			return false;
+		
+		// TODO
+		// Vérifier si le equals de List ne fait pas déjà ça
+		// Voir si moyen d'implémenter un iterator
+		List<Deplacement> deplacementsCopie = enfant.getDeplacements();
+		for(int i=0; i<deplacements.size(); i++){
+			if(deplacements.get(i) != deplacementsCopie.get(i))
+				return false;
+		}
+		
+		return true;
 	}
 }
