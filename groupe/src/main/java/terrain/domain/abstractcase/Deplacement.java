146f278aa0ecabeb5570f4afa7231811aea9bb16
Celande <celande.adrien@gmail.com>
2017-04-02 19:55:59 +0200

    Implémentation de deLettreADeplacement

--- groupe/src/main/java/terrain/domain/abstractcase/Deplacement.java
+++ groupe/src/main/java/terrain/domain/abstractcase/Deplacement.java
@@ -1,7 +1,25 @@
 package terrain.domain.abstractcase;
 
+import org.apache.log4j.Logger;
+
 public enum Deplacement {
 	AVANT,
 	GAUCHE,
 	DROITE;
+	
+	public static Deplacement deLettreADeplacement(char c) throws UnsupportedOperationException{
+		switch(c){
+		case 'a':
+		case 'A':
+			return AVANT;
+		case 'g':
+		case 'G':
+			return GAUCHE;
+		case 'd':
+		case 'D':
+			return DROITE;
+		}
+		
+		throw new UnsupportedOperationException("La lettre " + c + " ne correspond à aucun déplacement.");
+	}
 }
