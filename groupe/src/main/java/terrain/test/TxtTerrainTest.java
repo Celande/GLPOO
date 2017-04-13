51c9f3f38daaeda0027fd94e5ce0c0a3b9d4f2f0
xavier <xavier@xavier-E403SA>
2017-04-13 13:18:31 +0200

    Rajout fichier terrain, plus préparation aux tests d'init

--- groupe/src/main/java/terrain/test/TxtTerrainTest.java
+++ groupe/src/main/java/terrain/test/TxtTerrainTest.java
@@ -4,10 +4,18 @@ import org.apache.log4j.Logger;
 
 public class TxtTerrainTest extends AbstractTxtTerrainTest {
     private static final Logger LOGGER = Logger.getLogger(TxtTerrainTest.class);
+    
+    private final static String RESOURCES_PATH = "src/test/resources/";
+	private final static String TERRAIN_FILE_NAME = "terrain-01.txt";
+	private final static String ENFANT_FILE_NAME = "enfant-01.txt";
+    
+    public TxtTerrain terre;
 
     public TxtTerrainTest() {
         LOGGER.debug("Constructeur...");
 
         terre = new TxtTerrain();
+        TxtTerrain.init(new File(RESOURCES_PATH + TERRAIN_FILE_NAME),new File(RESOURCES_PATH + ENFANT_FILE_NAME));
+        
     }
 }
